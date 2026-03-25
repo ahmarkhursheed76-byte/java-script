3fda67f8e772c23e590e18018b3162982239a3c4
ahmarkhursheed76-byte <ahmarkhursheed76@gmail.com>
2026-03-25 16:47:09 +0000

    push it

---
+++ basics/nums_maths.js
@@ -0,0 +1,20 @@
+const scorecard =658
+//console.log( scorecard);
+
+const number1=354.856
+//console.log(number1.toPrecision(3));
+//console.log(Math.floor(4.87));
+//console.log(Math.round(6.98));
+//console.log(Math.max(4,6,5));
+//console.log(Math.min(6,4,9));
+//console.log(Math.random());
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
