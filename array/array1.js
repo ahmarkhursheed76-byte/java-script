84639c3a83bb65fcdd0f1837a20261a3456cd482
ahmarkhursheed76-byte <ahmarkhursheed76@gmail.com>
2026-03-29 11:07:53 +0000

    push it

---
+++ array/array1.js
@@ -0,0 +1,21 @@
+const myArr=[54,9,5,3,26]
+
+//console.log(myArr[3]);
+
+//slice and splice
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+
+
+console.log("B",myArr);
+const myn2=myArr.splice(1,3)
+console.log("C",myArr);
+
+console.log(myn2);
+
+
+
+
