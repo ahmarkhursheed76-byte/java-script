3519516e71e29cfa790c59fe2144a9c0777f698c
ahmarkhursheed76-byte <ahmarkhursheed76@gmail.com>
2026-03-29 10:24:35 +0000

    push it

---
+++ basics/date_time.js
@@ -0,0 +1,35 @@
+let myDate=new Date()
+
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+
+//let myCreatedDate=new Date(2023,0,23)
+let myCreatedDate= new Date(2024,6,16,7,4)
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+//console.log(Math.floor(Date.now()/1000));
+
+
+let newDate=new Date()
+console.log(newDate.toLocaleString());
+console.log(newDate.getDate());
+console.log(newDate.getMonth() + 1 );
+console.log(newDate.getDay());
+console.log(newDate.getMinutes());
+console.log(newDate.getMilliseconds());
+console.log(newDate.getHours());
+
+
+
+
+
+
+
+
+
+
+
